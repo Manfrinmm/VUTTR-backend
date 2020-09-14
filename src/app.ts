df7f9c9e407680c4de6fc5da08371320b99df163
Manfrinmm <matheus_mm123@hotmail.com>
2020-09-14 20:03:48 -0300

    docs: add swagger docs

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,8 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(express.static("docs/documentation/swagger"));
+
 app.use(errors());
 
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
